07c84ff45e8a08f525f2b630a78709206caaed13
Vinay <vinaypratap0455@gmail.com>
2025-12-14 02:29:31 +0530

    Add SQL query to find distinct numbers that appear three times consecutively

---
+++ 189_consecutive_Numbers.sql
@@ -0,0 +1,12 @@
+/* 
+This query uses two self-joins to align three consecutive rows
+ by their id values, then filters to ensure the num column
+  is identical across those rows. Finally, it selects the 
+  distinct numbers that appear three times in a row.
+ */
+
+ SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM  Logs l1
+JOIN  Logs l2 ON l1.id + 1=l2.id
+JOIN  Logs l3 ON l2.id + 1=l3.id
+WHERE l1.num=l2.num AND l2.num=l3.num;
