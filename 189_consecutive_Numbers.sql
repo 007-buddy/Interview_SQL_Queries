ce7b74a86bba5dc21daf375d8c5b0637c769875f
Vinay <vinaypratap0455@gmail.com>
2025-12-14 16:54:57 +0530

    Add alternative SQL query to find consecutive numbers using GROUP BY and HAVING clause

--- 189_consecutive_Numbers.sql
+++ 189_consecutive_Numbers.sql
@@ -11,3 +11,10 @@ FROM  Logs l1
 JOIN  Logs l2 ON l1.id + 1=l2.id
 JOIN  Logs l3 ON l2.id + 1=l3.id
 WHERE l1.num=l2.num AND l2.num=l3.num;
+
+-- Another approach using group by and having clause
+
+ SELECT Num as ConsecutiveNums
+  FROM Logs
+ GROUP BY Num
+ HAVING COUNT(*)>3;
